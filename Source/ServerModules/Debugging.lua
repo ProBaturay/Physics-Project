9458f353c8764ca3af3294b697df77be423f3e52
ProBaturay <99765925+ProBaturay@users.noreply.github.com>
2023-01-11 01:47:43 +0300

    Create Debugging.lua

---
+++ Source/ServerModules/Debugging.lua
@@ -0,0 +1,14 @@
+--[[
+    ÖNEMLİ NOT: Bu komut dosyası, TÜBİTAK kurumunun yarışması çerçevesinde projeye katkı sağlaması amacıyla yazılmış olup projenin simülasyonunu hedefleyen bir çeşit programlama dili içerir. Eğer bu yazıyı görüyorsanız, lütfen proje sahiplerine danışınız.
+    
+    IMPORTANT NOTE: This script was written to contribute to a project within the framework of the competition of the TÜBİTAK institution (The Scientific and Technological Research Council of Türki̇ye) and comprises a kind of programming language with which project's simulation will be carried out. Should you see this writing please consult with the project's owners.
+    
+    Oluşturulma Tarihi / Creation Date : +03:00 16/11/2022 18:53
+    Yazar / Writer                     : @ProBaturay
+--]]
+
+--!strict
+	
+local debugging = {}
+
+return debugging
